b9cdfb4f8fd8c34d060d7160f639584e74a7104d
Popescu Cristian <cristian.popescu@bitstone.eu>
2022-08-15 21:31:58 +0300

    Last version Day1 + Day2

--- array.php
+++ array.php
@@ -69,6 +69,7 @@ array_splice( $array7, $position, 0, $inserted_value );
 foreach ($array7 as $item){
     echo $item.' ';
 }
+//8
 echo nl2br("\n8. Write a PHP script to sort the following associative array :");
 $array8 = array("Sophia"=>"31","Jacob"=>"41","William"=>"39","Ramesh"=>"40");
 echo nl2br("\na) ascending order sort by value\n");
@@ -84,6 +85,7 @@ echo nl2br("\nd) descending order sorting by Key\n");
 krsort($array8);
 printArrValKey($array8);
 
+//9
 echo nl2br("\n9. Write a PHP script to calculate and display average temperature, five lowest and highest temperatures.\n");
 $array9 = [78, 60, 62, 68, 71, 68, 73, 85, 66, 64, 76, 63, 75, 76, 73, 68, 62, 73, 72, 65, 74, 62, 62, 65, 64, 68, 73, 75, 79, 73];
 sort($array9);
@@ -99,5 +101,184 @@ foreach(array_slice($unique9, -7, 7) as $item){
     echo " ".$item.",";
 }
 
+//11
+echo nl2br("\n11. Write a PHP program to merge (by index) the following two arrays.\n");
+$array11_1 = array(array(77, 87), array(23, 45));
+$array11_2 = array("w3resource", "com");
+foreach ($array11_2 as $key=>$item){
+    array_unshift($array11_1[$key], $item);
+}
+print_r($array11_1);
 
+//12
+echo nl2br("\n12. Write a PHP function to change the following array's all values to upper or lower case.\n");
+echo nl2br("Values are in lower case.\n");
+$Color12 = array('A' => 'Blue', 'B' => 'Green', 'c' => 'Red');
+print_r(array_map('strtolower', $Color12));
+echo nl2br("\nValues are in upper case.\n");
+print_r(array_map('strtoupper', $Color12));
 
+//13
+echo nl2br("\n13.Write a PHP script which display all the numbers between 200 and 250 that are divisible by 4.\n");
+print_r(range(200,250, 4));
+//14
+echo nl2br("\n14. Write a PHP script to get the shortest/longest string length from an array.\n");
+function sort_by_length($a,$b){
+    return strlen($b)-strlen($a);
+}
+$array14 = array("abcd","abc","de","hjjj","g","wer");
+usort($array14,'sort_by_length');
+echo "The shortest array length is ".strlen($array14[0])." The longest array length is ".strlen(end($array14));
+
+//15
+echo nl2br("\n15. Write a PHP script to generate unique random numbers within a range.\n");
+$array15 = [];
+$start15 = 11;
+$end15 = 20;
+while (count($array15) <= $end15-$start15){
+    $rand_numer = rand($start15, $end15);
+    if (!in_array($rand_numer, $array15)) {
+        echo $rand_numer . ' ';
+        array_push($array15, $rand_numer);
+    }
+}
+
+//16
+echo nl2br("\n16. Write a PHP script to get the largest key in an array.\n");
+$arr16 = array( "Italy"=>"Rome", "Luxembourg"=>"Luxembourg", "Belgium"=> "Brussels",
+    "Denmark"=>"Copenhagen", "Finland"=>"Helsinki", "France" => "Paris", "Slovakia"=>"Bratislava",
+    "Slovenia"=>"Ljubljana", "Germany" => "Berlin", "Greece" => "Athens", "Ireland"=>"Dublin",
+    "Netherlands"=>"Amsterdam", "Portugal"=>"Lisbon", "Spain"=>"Madrid", "Sweden"=>"Stockholm",
+    "United Kingdom"=>"London", "Cyprus"=>"Nicosia", "Lithuania"=>"Vilnius", "Czech Republic"=>"Prague", "Estonia"=>"Tallin", "Hungary"=>"Budapest", "Latvia"=>"Riga",
+    "Malta"=>"Valetta", "Austria" => "Vienna", "Poland"=>"Warsaw") ;
+echo max(array_keys($arr16));
+
+//17
+echo nl2br("\n17. Write a PHP function that returns the lowest integer that is not 0\n");
+$arr17 = [12, 3, 45, 0 ,6, 0, 36];
+echo min(array_diff($arr17, array(0)));
+
+//18
+echo nl2br("\n18. Write a PHP function to floor decimal numbers with precision.
+Note: Accept three parameters number, precision and separator
+Sample Data :
+1.155, 2, "."
+100.25781, 4, "."
+-2.9636, 3, "."
+\n");
+function  floor_decimal_numbers($number, $precision, $separator){
+    $str = strval($number);
+    return substr($str, 0, stripos($str, $separator)+$precision+1);
+
+}
+echo nl2br(floor_decimal_numbers(1.155, 2, ".")."\n");
+echo nl2br(floor_decimal_numbers(100.25781, 4, ".")."\n");
+echo nl2br(floor_decimal_numbers(-2.9636, 3, ".")."\n");
+
+//19
+echo nl2br("\n19. Write a PHP script to print 'second' and Red from the following array.\n");
+$color19 = array ( "color" => array ( "a" => "Red", "b" => "Green", "c" => "White"),
+    "numbers" => array ( 1, 2, 3, 4, 5, 6 ),
+    "holes" => array ( "First", 5 => "Second", "Third"));
+echo $color19["holes"][5]."\n"; // prints "second"
+echo $color19["color"]["a"]."\n"; // prints "Red"
+
+//21
+echo nl2br("\n21. Write a PHP function to sort subnets\n");
+
+function subnets($subnet){
+    $sortSubset = $subnet;
+    sort($sortSubset, SORT_STRING );
+    return $sortSubset;
+}
+$subnet_list =
+    array('192.169.12',
+        '192.167.11',
+        '192.169.14',
+        '192.168.13',
+        '192.167.12',
+        '122.169.15',
+        '192.167.16'
+    );
+$sortSubset = subnets($subnet_list);
+
+foreach ($sortSubset as $item){
+    echo nl2br($item."\n");
+}
+
+//23
+echo nl2br("\n21. Write a PHP function to sort subnets\n");
+
+$array23 = array();
+$array23[0]['name'] = 'Sana';
+$array23[0]['email'] = 'sana@example.com';
+$array23[0]['phone'] = '111-111-1234';
+$array23[0]['country'] = 'USA';
+
+$array23[1]['name'] = 'Robin';
+$array23[1]['email'] = 'robin@example.com';
+$array23[1]['phone'] = '222-222-1235';
+$array23[1]['country'] = 'UK';
+
+$array23[2]['name'] = 'Sofia';
+$array23[2]['email'] = 'sofia@example.com';
+$array23[2]['phone'] = '333-333-1236';
+$array23[2]['country'] = 'India';
+$sort_column = 'name';
+
+echo nl2br("\n23. Write a PHP program to sort a multi-dimensional array set by specific key\n");
+array_multisort( array_column($array23, $sort_column), SORT_ASC, $array23);
+print_r($array23);
+
+//24
+echo nl2br("\n24. Write a PHP script to sort an array using case-insensitive natural ordering.\n");
+$array1 = $array2 = array('IMG0.png', 'img12.png', 'img10.png', 'img2.png', 'img1.png', 'IMG3.png');
+natcasesort($array2);
+echo "\nNatural order sorting (case-insensitive)\n";
+print_r($array2);
+
+//26
+echo nl2br("\n26. Write a PHP function to shuffle an associative array, preserving key, value pairs.\n");
+$arr26 = array();
+$arr26[] = array('id' => 5, 'foo' => 'hello');
+$arr26[] = array('id' => 4, 'foo' => 'byebye');
+$arr26[] = array('id' => 9, 'foo' => 'foo');
+
+function shuffle_assoc($my_array){
+    $keys = array_keys($my_array);
+    $new = array();
+    shuffle($keys);
+    foreach ($keys as $key){
+        $new[] = $my_array[$key];
+    }
+    return $new;
+}
+print_r(shuffle_assoc($arr26));
+
+//27
+echo nl2br("\n27. Write a PHP function to generate a random password (contains uppercase, lowercase, numeric and
+other) using shuffle() function.\n");
+
+function rand_pass($num= 12){
+    $low_str = "abcdefghijklmnopqrstuvwxyz";
+    $up_str = strtoupper($low_str);
+    $num_str = "01234567";
+    $oth_str = "!@#$%^&*()_<>?=-+/`~";
+    $pass_string = str_repeat($low_str, 2).str_repeat($up_str, 2).str_repeat($num_str, 2).str_repeat($oth_str, 3);
+    $pass_array = str_split($pass_string);
+    shuffle($pass_array);
+    return implode(array_slice($pass_array, 0, $num));
+}
+print_r(rand_pass(12));
+
+//28
+echo nl2br("\n28. Write a PHP script to sort an array in reverse order (highest to lowest).\n");
+$arr28 = array("Red", "Orange", "Black", "White");
+rsort($arr28);
+print_r($arr28);
+
+//32
+echo nl2br("\n32. Write a PHP program to get the extension of a file.
+Example:example.txt'\n");
+$file32='example.txt';
+echo pathinfo($file32, PATHINFO_EXTENSION);
--- strings.php
+++ strings.php
@@ -2,7 +2,8 @@
 echo "<h1>Exercitii</h1>";
 
 echo "Strings";
-echo nl2br("1. Write a PHP script to : -\n");
+//1
+echo nl2br("\n1. Write a PHP script to : -\n");
 echo nl2br("a) transform a string all uppercase letters.\n");
 echo nl2br(strtoupper("Hello WORLD!")."\n");
 echo nl2br("b) transform a string all lowercase letters.\n");
@@ -12,12 +13,15 @@ echo nl2br(Ucfirst ("hello WORLD!")."\n");
 echo nl2br("d) make a string's first character of all the words uppercase.\n");
 echo nl2br(ucwords("welcome to geeks for geeks")."\n");
 
+//2
 echo nl2br("2. Write a PHP script to split the following string.
                  Sample string : '082307'
                  Expected Output : 08:23:07\n");
+$str2= '082307';
+echo substr(chunk_split($str2, 2, ':'), 0, -1)."\n";
 
-
-echo nl2br("3. Write a PHP script to check if a string contains specific string?
+//3
+echo nl2br("\n3. Write a PHP script to check if a string contains specific string?
                  Sample string : 'The quick brown fox jumps over the lazy dog.'
                  Check whether the said string contains the string 'jumps'.\n");
 if (strpos("The quick brown fox jumps over the lazy dog..", "jumps") !== false){
@@ -25,52 +29,64 @@ if (strpos("The quick brown fox jumps over the lazy dog..", "jumps") !== false){
 }else {
     echo "Not found!";
 }
+
+//4
 echo nl2br("\n4. Write a PHP script to convert the value of a PHP variable to string\n");
 $int_var = 12;
 $string_var = strval($int_var);
-
 echo "Old type ",  gettype($int_var), " new type ", gettype($string_var);
+
+//5
 echo nl2br("\n5.Write a PHP script to extract the file name from the following string.
                    Sample String : 'www.example.com/public_html/index.php'
                    Expected Output : 'index.php'\n");
 $str1 = 'www.example.com/public_html/index.php';
 echo substr($str1, strripos($str1, '/')+1);
 
+//6
 echo nl2br("\n6. Write a PHP script to extract the user name from the following email ID.
                    Sample String : 'rayy@example.com'
                   Expected Output : 'rayy'\n");
 $str6 = 'rayy@example.com';
 echo strstr($str6, '@', true);
 
+//7
 echo nl2br("\n7. Write a PHP script to get the last three characters of a string.\n");
 $str7 = 'rayy@example.com';
 echo substr($str7, strripos($str7, '.')+1);
 
+//8
 echo nl2br("\n8. Write a PHP script to format values in currency style.\n");
-
 $value1 = 64.45;
 $value2 = 104.35;
 echo number_format((float)($value1 + $value2), 2);
+
+//9
 echo nl2br("\n9. Write a PHP script to generate simple random password [do not use rand() function] from a given 
 string\n");
 $str9 = '1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZabcefghijklmnopqrstuvwxyz';
 echo substr(str_shuffle($str9),0, 8);
 
+//10
 echo nl2br("\n10. Write a PHP script to replace the first 'the' of the following string with 'That'.\n");
 $str10 = 'the quick brown fox jumps over the lazy dog.';
 echo preg_replace('/the/', 'That', $str10, 1);
 
+//11
 echo nl2br("\n11. Write a PHP script to find first character that is different between two strings.\n");
 $str11_1 = 'football';
 $str11_2 = 'footboll';
 $position = strspn($str11_1 ^ $str11_2, "\0");
 echo 5, ':', $str11_1[$position],' vs ', $str11_2[$position];
+
+//12
 echo nl2br("\n12. Write a PHP script to get the filename component of the following path.\n");
 $str12 = "http://www.w3resource.com/index.php";
 $start = strripos($str12, '/')+1;
 $end = strripos($str12, '.');
 echo substr($str12, $start,$end-$start);
 
+//13
 echo nl2br("\n13. Write a PHP script to print the next character of a specific character.\n");
 $char13 = 'z';
 $nextchar = ++$char13;
@@ -80,4 +96,86 @@ if (strlen($nextchar)>1){
     echo $nextchar;
 }
 
+//14
+echo nl2br("\n14. Write a PHP script to remove a part of a string from the beginning.
+Sample string : 'rayy@example.com'
+Expected Output : 'example.com'\n");
+$str14 = 'rayy@example.com';
+echo substr($str14, strripos($str14, '@')+1);
 
+//15
+echo nl2br("\n15. Write a PHP script to get a hex dump of a string.
+Sample string : 'rayy@example.com'\n");
+$str15 = 'rayy@example.com';
+echo bin2hex($str15)."\n";
+
+//16
+echo nl2br("\n16. Write a PHP script to insert a string at the specified position in a given string.
+Original String : 'The brown fox'
+Insert 'quick' between 'The' and 'brown'.
+Expected Output : 'The quick brown fox'\n");
+$original_string = 'The brown fox';
+$string_to_insert ='quick';
+$insert_pos = 4;
+$new_string = substr_replace($original_string, $string_to_insert.' ', $insert_pos, 0);
+echo $new_string."\n";
+
+//17
+echo nl2br("\n17. Write a PHP script to get the first word of a sentence.
+Original String : 'The quick brown fox'
+Expected Output : 'The'\n");
+echo strtok('The quick brown fox', ' ');
+
+//18
+echo nl2br("\n18. Write a PHP script to remove all leading zeroes from a string.
+Original String : '000547023.24'
+Expected Output : '547023.24'\n");
+echo ltrim('000547023.24',"0");
+
+//19
+echo nl2br("\n19. Write a PHP script to remove part of a string.
+Original String : 'The quick brown fox jumps over the lazy dog'
+Remove 'fox' from the above string.
+Expected Output : 'The quick brown jumps over the lazy dog'\n");
+echo str_replace('fox', '','The quick brown fox jumps over the lazy dog');
+
+//20
+echo nl2br("\n20. Write a PHP script to remove trailing slash from a string.
+Original String : 'The quick brown fox jumps over the lazy dog///'
+Expected Output : 'The quick brown fox jumps over the lazy dog'\n");
+$str20 = 'The quick brown fox jumps over the lazy dog///';
+echo rtrim($str20, '/');
+
+//21
+echo nl2br("\n21. Write a PHP script to get the characters after the last '/' in an url.
+Sample URL : 'http://www.example.com/5478631'
+Expected Output : '5478631'\n");
+echo ltrim(strrchr('http://www.example.com/5478631', '/'),'/');
+
+//22
+echo nl2br("\n22. Write a PHP script to replace multiple characters from the following string.
+Sample String : '1+2/3*2:2-3/4*3'
+Expected Output : '1 2 3 2 2 3 4 3'\n");
+$str22 = '\"\1+2/3*2:2-3/4*3';
+echo str_replace(str_split('"\+/*:-"'),"",$str22);
+
+//23
+echo nl2br("\n23. Write a PHP script to select first 5 words from the following string.
+Sample String : 'The quick brown fox jumps over the lazy dog'
+Expected Output : 'The quick brown fox jumps'\n");
+$str23 = 'The quick brown fox jumps over the lazy dog';
+echo implode(' ', array_slice(explode(' ', $str23), 0 ,5));
+
+//24
+echo nl2br("\n24. Write a PHP script to remove comma(s) from the following numeric string.
+Sample String : '2,543.12'
+Expected Output : 2543.12\n");
+echo str_replace(',',"",'2,543.12');
+//25
+echo nl2br("\n25. Write a PHP script to print letters from 'a' to 'z'.
+Expected Result : abcdefghijklmnopqrstuvwxyz\n");
+for ($x = ord('a'); $x <= ord('z'); $x++)
+    echo chr($x);
+echo "\n";
+echo nl2br("\n26. What will display the next code:\n");
+echo "Aop";
